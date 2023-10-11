785b0306b9d5621fcdfb05cc59f48fe55bf07c7d
hanaelmoatasem <hanakandil1610@gmail.com>
2023-10-12 01:14:31 +0300

    commit

--- backend/routes/clinic.js
+++ backend/routes/clinic.js
@@ -5,8 +5,8 @@ const appt1= require('../controllers/doctor');
 const appt2= require('../controllers/patient');
 
 
-router.get('/filter/doctor', doctor.filterAllApps);
-router.get('/filter/patient', patient.filterAllApps);
+router.get('/filter/doctor', appt1.filterAllApps);
+router.get('/filter/patient', appt2.filterAllApps);
 
 
 module.exports= router;
