48989b81ad41efe9e84fcdb2af9f87d0a4ee1ccf
hanaelmoatasem <hanakandil1610@gmail.com>
2023-12-17 00:53:58 +0200

    commit

--- frontend/src/components/VideoCallComponent.js
+++ frontend/src/components/VideoCallComponent.js
@@ -1,8 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
  
 const VideoCallComponent = () => {
+ 
   const [callId, setCallId] = useState(null);
+  const localVideoRef = useRef(null);
+  const remoteVideoRef = useRef(null);
+  let localStream = null;
+  let peerConnection = null;
 
   // Function to start a video call
   const startCall = async () => {
@@ -14,6 +19,21 @@ const VideoCallComponent = () => {
 
       const { callId } = response.data;
       setCallId(callId);
+      localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+      localVideoRef.current.srcObject = localStream;
+
+      // Create peer connection and add tracks
+      peerConnection = new RTCPeerConnection(/* configuration */);
+      localStream.getTracks().forEach(track => {
+        peerConnection.addTrack(track, localStream);
+      });
+
+      // Set up event handlers for peer connection
+      peerConnection.ontrack = event => {
+        remoteVideoRef.current.srcObject = event.streams[0];
+      };
+
+      // TODO: Add event handlers for ICE candidates, etc.
     } catch (error) {
       console.error('Error starting the call:', error);
       // Handle error, show error message, etc.
@@ -22,20 +42,34 @@ const VideoCallComponent = () => {
 
   // Function to end a video call
   const endCall = async () => {
-    try {
+   
       await axios.post('/api/patient/endcall', { callId });
 
-      // Clear the callId in the component state
+      if (peerConnection) {
+        peerConnection.close();
+        peerConnection = null;
+      }
+      if (localStream) {
+        localStream.getTracks().forEach(track => track.stop());
+        localStream = null;
+      }
+
+      if (localVideoRef.current) {
+        localVideoRef.current.srcObject = null;
+      }
+      if (remoteVideoRef.current) {
+        remoteVideoRef.current.srcObject = null;
+      }
       setCallId(null);
-    } catch (error) {
-      console.error('Error ending the call:', error);
-      // Handle error, show error message, etc.
-    }
+      
+   
   };
 
   return (
     <div>
       <h1>Video Call Component</h1>
+      <video ref={localVideoRef} autoPlay muted></video>
+      <video ref={remoteVideoRef} autoPlay></video>
       {callId ? (
         <div>
           <p>Call ID: {callId}</p>
