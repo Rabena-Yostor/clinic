0e886ab8daf69e2c9b8d9f1531401636b4a90cb2
hanaelmoatasem <hanakandil1610@gmail.com>
2023-12-16 21:56:15 +0200

    commit

--- frontend/src/components/RejectDoctorRequest.js
+++ frontend/src/components/RejectDoctorRequest.js
@@ -1,30 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaUserTimes } from 'react-icons/fa';
 
 const RejectDoctorRequest = () => {
-    const [doctorId, setDoctorId] = useState('');
+    const [pendingDoctors, setPendingDoctors] = useState([]);
     const [message, setMessage] = useState('');
-  
-    const handleDoctorIdChange = (e) => {
-      setDoctorId(e.target.value);
-    };
-  
-    const handleRejectRequest = async (e) => {
-        e.preventDefault();
-    
+
+    useEffect(() => {
+        const fetchPendingDoctors = async () => {
+            try {
+                const response = await fetch('http://localhost:4000/api/admin/viewRequests');
+                const data = await response.json();
+                if (response.ok) {
+                    setPendingDoctors(data);
+                } else {
+                    setMessage(data.error);
+                }
+            } catch (error) {
+                console.error('Error fetching pending doctors:', error);
+                setMessage('Internal Server Error');
+            }
+        };
+
+        fetchPendingDoctors();
+    }, []);
+
+    const handleRejectRequest = async (doctorId) => {
         try {
             const response = await fetch(`http://localhost:4000/api/admin/rejectRequests`, {
                 method: 'DELETE',
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({ doctorId }), // Include the doctorId in the request body
+                body: JSON.stringify({ doctorId }),
             });
-    
+
             const data = await response.json();
-    
             if (response.ok) {
                 setMessage(data.message);
+                setPendingDoctors(pendingDoctors.filter(doctor => doctor._id !== doctorId));
             } else {
                 setMessage(data.error);
             }
@@ -34,37 +47,29 @@ const RejectDoctorRequest = () => {
         }
     };
 
-  return (
-    <>
-      <section className="Heading">
-        <h1>
-          <FaUserTimes /> Reject Doctor Request
-        </h1>
-        <p>Enter doctor ID to reject the request.</p>
-      </section>
+    return (
+        <>
+            <section className="Heading">
+                <h1>
+                    <FaUserTimes /> Reject Doctor Request
+                </h1>
+                <p>Select a doctor request to reject.</p>
+            </section>
 
-      <section className="form">
-        <form onSubmit={handleRejectRequest}>
-          <div className="form-group">
-            <label htmlFor="doctorId">Doctor ID:</label>
-            <input
-              type="text"
-              className="form-control"
-              id="doctorId"
-              name="doctorId"
-              value={doctorId}
-              placeholder="Enter Doctor ID"
-              onChange={handleDoctorIdChange}
-            />
-          </div>
-          <button type="submit" className="btn btn-primary">
-            Reject Request
-          </button>
-        </form>
-        {message && <p className={message.includes('successfully') ? 'success-message' : 'error-message'}>{message}</p>}
-      </section>
-    </>
-  );
+            <section className="form">
+    {pendingDoctors.map(pendingDoctor => (
+        <div key={pendingDoctor._id} className="pending-doctor">
+            <p>{pendingDoctor.name} - ID: {pendingDoctor._id}</p> {/* Directly access name from pendingDoctor */}
+            <button onClick={() => handleRejectRequest(pendingDoctor._id)} className="btn btn-primary">
+                Reject Request
+            </button>
+        </div>
+    ))}
+    {message && <p className={message.includes('successfully') ? 'success-message' : 'error-message'}>{message}</p>}
+</section>
+
+        </>
+    );
 };
 
 export default RejectDoctorRequest;
